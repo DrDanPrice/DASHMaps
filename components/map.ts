ae4eb790f8ba7e96e6c3550085b2035a0a4aea6a
DrDanPrice <dr.dan.price@gmail.com>
2016-06-26 21:18:31 -0500

    not sure - observables don't do it in the right order, either...

--- components/map.ts
+++ components/map.ts
@@ -2,6 +2,7 @@
 import { Component } from '@angular/core';
 import { OnActivate, RouteSegment } from '@angular/router';
 import { MapStyleService } from '../services/mapstyles_service.ts';
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: 'map-display',
@@ -17,18 +18,35 @@ export class MapComponent implements OnActivate{
   //  super()
   }
   routerOnActivate(curr: RouteSegment): void {
+
     let stylename:string = curr.getParam('localmapstylename');
-    console.log(stylename)
+    //console.log(stylename)
+
     // let stylename = curr.getParam('localmapstylename');
     //
     //
     //if (stylename==undefined){stylename='default';console.log('adfsdfs')}
-    //this.MapStyleService.setWorkingMapSetting(stylename);
+    this.MapStyleService.setWorkingMapSetting(stylename);
     //console.log('routerOnActivate getParam() in map',curr.getParam('localmapstylename'))
     //this.getMapSettings()
     //this should write out to the service, which should determine the mapstyles to show
     //should treat getParam as a promise?
   }
+
+  // getWorkMapSet(newSet:Array<any>):Observable {
+  //
+  //     let obs = this.todoBackendService.saveTodo(newTodo);
+  //
+  //     obs.subscribe(
+  //             res => {
+  //                 this._todos.next(this._todos.getValue().push(newTodo));
+  //             },
+  //             err => console.log("Error pushing new")
+  //           );
+  //
+  //     return obs;
+  // }
+
   mapstyle:any;
   getMapSettings () {
     this.MapStyleService.getDefaultSettings().then(mapstyle => {
